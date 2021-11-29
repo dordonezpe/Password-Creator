c07666efaf3bb807fe5f6abc1c2803ff299214e3
dordonezpe <dordonezpe@gmail.com>
2021-11-29 09:09:28 -0500

    Done, finally done

--- pwc.py
+++ pwc.py
@@ -14,50 +14,86 @@ def createAlphabets():
     atbashAlphabet = lowerAlphabet[:]
     atbashAlphabet += upperAlphabet[:]
 
-    a1z26, finalList = [], []
+    a1z26 = []
     [a1z26.append(number+1) for number in range(0, 26)]
     [a1z26.append(number+1) for number in range(0, 26)]
 
 #There's no supervillian without a presentation
 def presentation():
-    phrase = input("Enter a phrase: ")
+    global phrase
+    print("____________________________________________________________________________")
+    print("\n\t\tWelcome to the Password Creator program")
+    print("____________________________________________________________________________")
+    print("\nPlease, enter a phrase ")
+    phrase = input(": ")
 
 #Adjusments
-a,b = 'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN'
-trans = str.maketrans(a,b)
-phrase = phrase.translate(trans)
+def adjustments():
+    global phrase
+    a,b = 'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN'
+    trans = str.maketrans(a,b)
+    phrase = phrase.translate(trans)
 
 #Change string to list
-lettersList = [char for char in phrase]
+def stringToList(): 
+    global lettersList
+    lettersList = [char for char in phrase]
 
 #Index number in the alphabet
-for letter in lettersList: 
-    if letter.isspace(): pass
-    else: 
-        letterInAlphabet = alphabet.index(letter)
-        indexNumber = letterInAlphabet+1
+def core():
+    global finalList
+    finalList = []
+    for letter in lettersList: 
+        if letter.isspace(): pass
+        else: 
+            letterInAlphabet = alphabet.index(letter)
+            indexNumber = letterInAlphabet+1
+            
+        #Create random decision
+        letter_Number = random.randint(0, 6)
         
-    #Create random decision
-    letter_Number = random.randint(0, 4)
-    print(f"Random Number: {letter_Number}")
+        #Letter
+        if letter_Number >= 1:
+            if letter.isspace(): toAssign = " "
+            else: toAssign = atbashAlphabet[indexNumber-1]
+            
+        #Number
+        if letter_Number == 0: 
+            if letter.isspace():  toAssign = " "
+            else: toAssign = a1z26[indexNumber-1]
+
+        finalList.append(str(toAssign))
+
+#List to String
+def listToString(wordList):
+    global listToStr 
+    finalList.insert(0, "@")
+    listToStr = ""
+    listToStr += ''.join(str(element) for element in wordList)
+    listToStr = listToStr.replace(" ", "")
+    return listToStr
+
+presentation()
+createAlphabets()
+adjustments()
+stringToList()
+core()
+
+#Process
+while True: 
     
-    #Letter
-    if letter_Number >= 1:
-        if letter.isspace(): toAssign = " "
-        else: toAssign = atbashAlphabet[indexNumber-1]
-        
-    #Number
-    if letter_Number == 0: 
-        if letter.isspace():  toAssign = " "
-        else: toAssign = a1z26[indexNumber-1]
-
-    finalList.append(str(toAssign))
-
-finalList.insert(0, "@")
-listToStr = ""
-listToStr += ''.join(str(element) for element in finalList)
-listToStr = listToStr.replace(" ", "")
-
-print("Here's your password: ")
-print(listToStr)
+    print("Here's your password: ")
+    print(listToString(finalList))
+    
+    print("\nChoose 'yes' / 'no' ")
+    option = input("Would you like to refresh the password: ")
+    option = option.lower()
+    option = option.strip()
+    
+    if option == "yes" or option == "y": 
+        core()
+        continue
+    if option == "no" or option == "n": 
+        print("Thank you for use our program <3")
+        break
 
